91b5d0ce8e40f160baf8c153d681582ff7eb4e85
AdnanK1 <adnangitonga@gmail.com>
2022-05-04 01:06:50 +0300

    Added get_news function to routes to allow display of news in home.html .

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import main
+from ..request import get_news
 
 @main.route('/')
 @main.route('/home')
 def home_page():
-    return render_template('home.html')
+    news = get_news()
+    return render_template('home.html', articles = news)
